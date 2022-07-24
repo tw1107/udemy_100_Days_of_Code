b09b6be9f7cd6ca9a427a6a87503ebb13fc5c8db
Alice <tw1107@nyu.edu>
2022-07-24 10:28:34 -0700

    day-24-work with files

---
+++ day-24-work_with_files/main.py
@@ -0,0 +1,10 @@
+with open("/Users/a.b.wang/Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# with open("my_file.txt", "a") as file:
+#     file.write("\nNew text.")
+
+#
+# with open("new_file.txt", "w") as file:
+#     file.write("New text.")
